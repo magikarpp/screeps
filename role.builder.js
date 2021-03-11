02fb0f926670684a9b3d64a5293179ada73c16b0
magikarp <shimjay1@gmail.com>
2021-03-11 11:01:36 -0500

    working builder

---
+++ role.builder.js
@@ -0,0 +1,59 @@
+let util = require("util");
+
+let builder = {
+  defaultStrat: function (creep) {
+    if (creep.memory.isWorking) {
+      creep.memory.source = "none";
+
+      let target = creep.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES);
+
+      if (target) {
+        creep.memory.target = target.id;
+
+        if (creep.build(target) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target, { visualizePathStyle: { stroke: "#ffffff" } });
+        }
+      } else {
+        creep.memory.role = "upgrader";
+      }
+    } else {
+      let source;
+
+      if (creep.memory.source == "none") {
+        source = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+          filter: (structure) => {
+            return (
+              (structure.structureType == STRUCTURE_EXTENSION ||
+                structure.structureType == STRUCTURE_SPAWN ||
+                structure.structureType == STRUCTURE_CONTAINER ||
+                structure.structureType == STRUCTURE_STORAGE) &&
+              structure.energy > 40
+            );
+          },
+        });
+      } else {
+        source = Game.getObjectById(creep.memory.source);
+      }
+
+      if (source) {
+        creep.memory.source = source.id;
+        creep.memory.target = "none";
+
+        if (creep.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(source, { visualizePathStyle: { stroke: "#ffaa00" } });
+        }
+        if (creep.carry.energy != creep.carryCapacity) {
+          creep.memory.source = "none";
+        }
+      }
+    }
+  },
+  run: function (creep, strat) {
+    if (strat == "default") {
+      this.defaultStrat(creep);
+    } else if (strat == "") {
+    }
+  },
+};
+
+module.exports = builder;
